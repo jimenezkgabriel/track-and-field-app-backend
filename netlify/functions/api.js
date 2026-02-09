2b3228a07e825d8439860b4b3ca232061b62a916
jimenezkgabriel <jimenez.k.gabriel@gmail.com>
2026-02-09 12:16:15 -0500

    altered response for testing

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,7 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/hello", (req, res) => res.send({ message: "Hello, World!" }));
 
 api.use("/api/", router);
 
